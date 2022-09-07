3af356cf6bef64a1743a2aea2351dfd044e70db6
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-07 09:42:31 +0200

    Definizione array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 console.log('JS OK!');
+
+const teamMembers = [
+    {
+        name: 'Wayne Barrett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log('Team Members array: ', teamMembers)
